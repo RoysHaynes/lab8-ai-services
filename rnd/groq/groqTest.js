91b34d2520a41233bf163acec1d2c1c90b4087d4
roysh <rhaynes@sandiego.edu>
2025-10-28 22:31:51 -0700

    Feat: completed testing groq

---
+++ rnd/groq/groqTest.js
@@ -0,0 +1,25 @@
+document.getElementById("send").addEventListener("click", function(){
+    let msg = document.getElementById("msg").value;
+    let api_key = prompt("Enter key Groq: ");
+    let endpoint="https://api.groq.com/openai/v1/chat/completions";
+
+    fetch(endpoint, {
+        method: "POST",
+        headers:{
+            "Authorization": `Bearer ${api_key}`,
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            model:"llama-3.3-70b-versatile",
+            messages:[
+                {role:'user',content:msg}
+            ]
+        })
+    })
+        .then(function(response){return response.json();})
+        .then(function(data){
+            let p = document.createElement("p");
+            p.textContent = data.choices[0].message.content;
+            document.body.appendChild(p);
+        });
+});
